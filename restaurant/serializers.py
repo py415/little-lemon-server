4459948fcd9debc05cbb689247a5d82b0c679d01
py415 <philipyu1997@gmail.com>
2023-06-27 19:12:25 -0700

    Add serializers for booking, menu, and user models

---
+++ restaurant/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Booking, Menu
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ["id", "name", "no_of_guests", "booking_date"]
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ["id", "title", "price", "inventory"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "url", "username", "email", "groups"]
